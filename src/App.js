a45889a12efcc8c1bf141c521b1e59f51e1ae217
Kevin Luo <kvn.luo@gmail.com>
2020-10-25 16:17:27 -0400

    add pusher functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import Pusher from 'pusher-js';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('737fbac157154505f9c2', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages'); // messages comes from pusher.trigger in server.js
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app_body">
